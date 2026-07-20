735a4ca8d7cb1c9223b0b6c53f8e3daa8b3121e5
Vladyslav Palamarchuk <fayttter@gmail.com>
2026-07-20 22:36:18 +0300

    create context fix 3

--- app/main.py
+++ app/main.py
@@ -1,25 +1,13 @@
 import os
-from types import TracebackType
 
 
 class CleanUpFile:
-    # write your code here
-    pass
-
     def __init__(self, filename: str) -> None:
         self.filename = filename
-        self.file = None
 
-    def __enter__(self) -> object:
-        self.file = open(self.filename, "w")
+    def __enter__(self) -> "CleanUpFile":
         return self
 
-    def __exit__(
-            self,
-            exc_type: type[BaseException] | None,
-            exc_val: BaseException | None,
-            exc_tb: TracebackType | None
-    ) -> None:
-        self.file.close()
+    def __exit__(self, typical: str, value: int, traceback: str) -> None:
         if os.path.exists(self.filename):
             os.remove(self.filename)
